44e1fead97e761788074a7219154fd8cc2cfe4dc
ELKAISSI SOUHAIL <elkaissimath@gmail.com>
2017-04-01 13:12:17 +0100

    V_0.9

--- application/applicationMain.h
+++ application/applicationMain.h
@@ -27,7 +27,8 @@ Fenetre *f = new_Fenetre("ma premiere fenetre",NORMAL,new_Taille(500,500),P_CENT
     c1 = Paned_add1(c1,img->this);
     c1 = Paned_add2(c1,i->this);
 
-                                                
+ ALL_setForegroundColor(c1->this,0.2,0.9,0.9,1);         
+ ALL_setBackgroundColor(c1->this,0.8,0.8,0.4,1);                                
 
     Fenetre_setVisible(f,1);
    
