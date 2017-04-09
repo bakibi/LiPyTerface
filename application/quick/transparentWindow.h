3d20aca2d6df4663a779ffa529bd9e09068593cf
ELKAISSI SOUHAIL <elkaissimath@gmail.com>
2017-04-09 13:28:28 +0100

    x

--- application/quick/transparentWindow.h
+++ application/quick/transparentWindow.h
@@ -1,29 +1,13 @@
 static void screen_changed(GtkWidget *widget, GdkScreen *old_screen, gpointer user_data);
-static gboolean draw(GtkWidget *widget, cairo_t *new_cr, gpointer user_data);
-static void clicked(GtkWindow *win, GdkEventButton *event, gpointer user_data);
 
 Fenetre* new_transparent_fenetre()
 {
     Fenetre *trans = new_Fenetre("",NORMAL,new_Taille(100,100),P_NONE);
-    gtk_window_set_decorated(GTK_WINDOW(trans->this),FALSE);
-
+    g_signal_connect(G_OBJECT(trans->this), "delete-event", gtk_main_quit, NULL);
     gtk_widget_set_app_paintable(trans->this, TRUE);
-
-    g_signal_connect(G_OBJECT(trans->this), "draw", G_CALLBACK(draw), NULL);
     g_signal_connect(G_OBJECT(trans->this), "screen-changed", G_CALLBACK(screen_changed), NULL);
-
     gtk_window_set_decorated(GTK_WINDOW(trans->this), FALSE);
-    gtk_widget_add_events(trans->this, GDK_BUTTON_PRESS_MASK);
-    g_signal_connect(G_OBJECT(trans->this), "button-press-event", G_CALLBACK(clicked), NULL);
-
-    GtkWidget* fixed_container = gtk_fixed_new();
-    gtk_container_add(GTK_CONTAINER(trans->this), fixed_container);
-    GtkWidget* button = gtk_button_new_with_label("button1");
-    gtk_widget_set_size_request(button, 100, 100);
-    gtk_container_add(GTK_CONTAINER(fixed_container), button);
-
     screen_changed(trans->this, NULL, NULL);
-   
     return (trans);
 }
 
@@ -32,7 +16,6 @@ static void screen_changed(GtkWidget *widget, GdkScreen *old_screen, gpointer us
 {
     GdkScreen *screen = gtk_widget_get_screen(widget);
     GdkVisual *visual = gdk_screen_get_rgba_visual(screen);
-
     if (!visual)
     {
         visual = gdk_screen_get_system_visual(screen);
@@ -40,21 +23,6 @@ static void screen_changed(GtkWidget *widget, GdkScreen *old_screen, gpointer us
     }
     else
         supports_alpha = TRUE;
+    
     gtk_widget_set_visual(widget, visual);
 }
-static gboolean draw(GtkWidget *widget, cairo_t *cr, gpointer userdata)
-{
-   cairo_t *new_cr = gdk_cairo_create(gtk_widget_get_window(widget));
-    if (supports_alpha)
-        cairo_set_source_rgba (new_cr, 0.5, 1.0, 0.50, 0); /* transparent */
-    else
-        cairo_set_source_rgb (new_cr, 1.0, 1.0, 1.0); /* opaque white */
-    cairo_set_operator (new_cr, CAIRO_OPERATOR_SOURCE);
-    cairo_paint (new_cr);
-    cairo_destroy(new_cr);
-    return FALSE;
-}
-static void clicked(GtkWindow *win, GdkEventButton *event, gpointer user_data)
-{
-    gtk_window_set_decorated(win, !gtk_window_get_decorated(win));
-} 
